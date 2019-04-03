d04f53386cde073fc2cb2f1199765dc9013200f0
Breanna McIntyre <bmcinty9@uwo.ca>
2019-04-03 15:38:45 -0400

    initial crawler commit

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from alphaNews.sentiment import sentiment
 
 def main(companyname):
     load = json.load(open("key.json", 'r'))
-    key = load["apiKey"]
-    company_results = getCompaniesData(key, companyname)
-    sentiment(company_results, key)
+    sentimentApiKey = load["sentitmentApiKey"]
+    searchApiKey = load["searchApiKey"]
+    
+    company_results = getCompaniesData(key, searchApiKey)
+    sentiment(company_results, sentimentApiKey)
 
 
 if __name__ == "__main__":
