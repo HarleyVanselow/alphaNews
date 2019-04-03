650192d56cbe75f062281b9fc35d69f9f294130d
jasmineyan <jasmine.yan@outlook.com>
2019-04-03 16:40:32 -0400

    getStockPrice

---
+++ alphaNews/stock.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+def getStockPrices(companyName, requestDate):
+
+    print("done")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 #
 from alphaNews.crawler import getCompaniesData
 from alphaNews.sentiment import sentiment
-
+from alphaNews.stock import getStockPrices
 
 def main(companyname):
     load = json.load(open("key.json", 'r'))
@@ -12,7 +12,7 @@ def main(companyname):
     company_results = getCompaniesData(key, companyname)
     withSentiment = sentiment(company_results, key)
     print(withSentiment)
-
+    price = getStockPrices(1,2)
 
 if __name__ == "__main__":
     main(sys.argv[1])
