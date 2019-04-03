5748880aef12e702e76dcb6467e342813bf0455d
harleyvanselow <code@harleyv.net>
2019-04-03 14:24:27 -0400

    Add sentiment analysis poc

--- alphaNews/crawler.py
+++ alphaNews/crawler.py
@@ -1,2 +1,11 @@
-def getText(key, companyName):
-    return ["This is some good news", "This is some bad news"]
+def getText(key, topic):
+    result = {}
+    result.topic = topic
+    result.companies = [
+        {'name': 'Samsung',
+         'articles': [{'date': 123874872, 'text': "Some text is good!"},
+                      {'date': 1235232, 'text': "Some text is bad!"}
+                      ]
+         }
+    ]
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import os
 import sys
 
 from alphaNews.crawler import getText
